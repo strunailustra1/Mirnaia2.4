10ccf489c30ccc2ac8c8922f297d4f85baf8d198
Natalia Mirnaia <strunailustra@gmail.com>
2019-11-24 22:09:43 +0300

    save button

---
+++ Mirnaia2.4/LanguageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LanguageViewController.swift
+//  Mirnaia2.4
+//
+//  Created by Наталья Мирная on 24/11/2019.
+//  Copyright © 2019 Наталья Мирная. All rights reserved.
+//
+
+import UIKit
+
+class LanguageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
